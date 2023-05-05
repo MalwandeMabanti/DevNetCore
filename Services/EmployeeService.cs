8bbcd74260e5259feaf210b2b7899070d36a885b
Malwande Mabanti <malwandem@payspace.com>
2023-05-05 16:22:06 +0200

    Add more employee entries

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -21,6 +21,76 @@
                     Id = 456,
                     Name = "Cristiano",
                     Age = 37
+                },
+
+                new Employee()
+                {
+                    Id = 789,
+                    Name = "Iniesta",
+                    Age = 40
+                },
+
+                new Employee()
+                {
+                    Id = 987,
+                    Name = "Xavi",
+                    Age = 43
+                },
+
+                new Employee()
+                {
+                    Id = 654,
+                    Name = "Busquets",
+                    Age = 39
+                },
+
+                new Employee()
+                {
+                    Id = 321,
+                    Name = "Ramos",
+                    Age = 41
+                },
+
+                new Employee()
+                {
+                    Id = 012,
+                    Name = "Rooney",
+                    Age = 42
+                },
+
+                new Employee()
+                {
+                    Id = 345,
+                    Name = "Puyol",
+                    Age = 46
+                },
+
+                new Employee()
+                {
+                    Id = 678,
+                    Name = "Bruno",
+                    Age = 29
+                },
+
+                new Employee()
+                {
+                    Id = 876,
+                    Name = "Haaland",
+                    Age = 24
+                },
+
+                new Employee()
+                {
+                    Id = 543,
+                    Name = "Mbappe",
+                    Age = 23
+                },
+
+                new Employee()
+                {
+                    Id = 210,
+                    Name = "Neymar",
+                    Age = 31
                 }
             };
         }
